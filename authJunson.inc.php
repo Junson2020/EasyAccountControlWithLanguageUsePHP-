1a9974481b5671d8007afedf726129bc91a1b005
Junson <114983215+Junson2020@users.noreply.github.com>
2023-04-26 15:14:54 +0800

    Delete authJunson.inc.php

--- authJunson.inc.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();
-if(isset($_SESSION['junsonlicense'])) { $junsonlicense=$_SESSION['junsonlicense']; } else { $junsonlicense=""; }
-if(isset($_SESSION['junsonlanguage'])) { $junsonlanguage=$_SESSION['junsonlanguage']; } else { $junsonlanguage=$JUNSON_DEFAULT_LANGUAGE; }
-?>
